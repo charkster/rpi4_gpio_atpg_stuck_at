3438fca48b6ca3e360bfaf4e2e75221201460302
charkster <31560568+charkster@users.noreply.github.com>
2022-10-06 19:32:13 -0700

    Update test_gpio_scan.py

--- test_gpio_scan.py
+++ test_gpio_scan.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
+import json
+from gpio_scan                       import gpio_scan
+from complr_patterns_scan_entry_only import * 
 
-from gpio_scan        import gpio_scan
-from complr_patterns  import * # this defines pattern_list 
+with open('pattern_list.json', 'r') as filehandle:
+	pattern_list = json.load(filehandle)
 
 gs = gpio_scan()
 gs.load_and_compare_list( pat_name='scan_entry', pattern=list_se, shift=False )
 
-for scan_pat in range(0,3504):
+for scan_pat in range(0,len(pattern_list)):
 	gs.load_and_compare_list( pat_name=str(scan_pat), pattern=pattern_list[scan_pat], shift=True )
-
